2b69335ddbf63bc1538a97341853c43499a66abe
Ryan Bushe <rbushe@athenahealth.com>
2016-08-03 15:43:31 -0400

    Adding repo

---
+++ repo/Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		bower: {
+			install: {}
+		},
+
+		pleeease: {
+			custom: {
+				options: {
+					browsers: [ "last 3 versions" ],
+					sass: true,
+
+					sourcemaps: {
+						map: {
+							inline: false
+						}
+					}
+				},
+
+				files: {
+					"ios7.min.css": "ios7.scss"
+				}
+			}
+		},
+
+		bump: {
+			options: {
+				tagName: "%VERSION%"
+			}
+		}
+	});
+
+	grunt.loadNpmTasks("grunt-bower-task");
+	grunt.loadNpmTasks("grunt-pleeease");
+
+	grunt.registerTask("default", [ "bower:install", "pleeease" ]);
+	grunt.registerTask("install", [ "bower:install" ]);
+};
---
+++ repo/depictions/com.bushe.MarkRead.php
@@ -0,0 +1,13 @@
+<?php
+    $name="MarkRead";
+    $description=array(
+    "This is a very simple package to change the Mark button in the bottom left corner of the Mail app to Mark Read",
+    "Selecting the Mark Read button will mark the selected messages as read instead of prompting you with an action sheet",
+    "There are no options to configure");
+    
+    $changelog=array(
+    "0.0.1-1", " - Initial release.");
+    
+    $devText="Visit Homepage";
+    $devLink="http://bushe.me";
+?>
